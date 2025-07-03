9a30390184c823c5cd1448c359895a45ff90ca59
josevictormoreno <josevictor.admoreno@gmail.com>
2025-07-03 06:40:58 -0300

    busca por todo um capítulo

--- search.py
+++ search.py
@@ -5,26 +5,42 @@ cursor = conn.cursor()
 print("bible search")
 print("1 - verse")
 print("2 - books")
+print("3 - chapter")
+
 opt = input("option: ")
+
 print("\n")
 
 if opt == "1":
-    book = input("book: ")
+    book = input("Abbreviation: ")
     chapter = int(input("chapter: "))
-    verse = int(input("verse: "))
+    verse_begin = int(input("verse begin: "))
+    verse_end = int(input("verse end: "))
     cursor.execute("""
         SELECT text FROM verses
-        WHERE book = ? AND chapter = ? AND verse = ?
-        """, (book, chapter, verse,))
+        WHERE abbrv = ? AND chapter = ? AND verse BETWEEN ? AND ?
+        """, (book, chapter, verse_begin, verse_end,))
     rows = cursor.fetchall()
 
     for row in rows:
         text = row
-        print(f"{book} {chapter}:{verse} - {text}")
+        print(f"{text}")
 
 if opt == "2":
     cursor.execute("SELECT DISTINCT book, abbrv FROM verses")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+
+if opt == "3":
+    book = input("Abbreviation: ")
+    chapter = input("chapter: ")
+    cursor.execute(
+        "SELECT text FROM verses WHERE abbrv = ? AND chapter = ?", (book, chapter,))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        i = 1
+        print(f"{i}:{row}")
+
 conn.close()
